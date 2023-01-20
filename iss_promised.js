c298e664fee012b5fcfca13ae66208305e4e24f7
Jason Divinagracia <jmarkd04@gmail.com>
2023-01-20 09:31:16 -0500

    added comments if ever I need to review promises on this project

--- iss_promised.js
+++ iss_promised.js
@@ -1,7 +1,44 @@
+const request = require('request-promise-native');
+
+/*
+ * Requests user's ip address from https://www.ipify.org/
+ * Input: None
+ * Returns: Promise of request for ip data, returned as JSON string
+ */
+
+const myUrl = 'https://api.ipify.org?format=json';
+const fetchMyIP = function() {
+  return request(myUrl);
+};
+
+/* 
+ * Makes a request to ipwho.is using the provided IP address to get its geographical information (latitude/longitude)
+ * Input: JSON string containing the IP address
+ * Returns: Promise of request for lat/lon
+ */
+
+const fetchCoordsByIP = function (body) {
+  const ip = JSON.parse(body).ip;
+  return request(`https://ipwho.is/${ip}`);
+};
+
+/*
+ * Requests data from https://iss-flyover.herokuapp.com using provided lat/long data
+ * Input: JSON body containing geo data response from ipwho.is
+ * Returns: Promise of request for fly over data, returned as JSON string
+ */
+
+const fetchISSFlyOverTimes = function (coords) {
+  const { latitude, longitude } = JSON.parse(coords);
+  const url = `https://iss-flyover.herokuapp.com/json/?lat=${latitude}&lon=${longitude}`;
+  return request(url)
+}
+
 /* 
 * Input: None
 * Returns: Promise for fly over data for users location
 */
+
 const nextISSTimesForMyLocation = function () {
   return fetchMyIP()
   .then(fetchCoordsByIP)
@@ -11,24 +48,5 @@ const nextISSTimesForMyLocation = function () {
     return response;
   });
 };
-
-const request = require('request-promise-native');
-
-const myUrl = 'https://api.ipify.org?format=json';
-const fetchMyIP = function() {
-  return request(myUrl);
-};
-
-const fetchCoordsByIP = function (body) {
-  const ip = JSON.parse(body).ip;
-  return request(`https://ipwho.is/${ip}`);
-};
-
-const fetchISSFlyOverTimes = function (coords) {
-  const { latitude, longitude } = JSON.parse(coords);
-  const url = `https://iss-flyover.herokuapp.com/json/?lat=${latitude}&lon=${longitude}`;
-  return request(url)
-}
-
 
 module.exports = { /*fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes, */nextISSTimesForMyLocation };
